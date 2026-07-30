8e685485a8e0125eb22fa5587605dd9873112e87
N33K <nik.kovalskiy17@gmail.com>
2026-07-30 10:27:39 +0300

    added f1 circuits

---
+++ f1-circuits-svg/svgo.config.mjs
@@ -0,0 +1,25 @@
+export default {
+  js2svg: {
+    indent: 4,
+    pretty: true,
+  },
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          removeViewBox: false, // avoid removing the viewBox attribute (important for scaling)
+          convertStyleToAttrs: false, // avoid converting styles to attributes
+          cleanupNumericValues: false, // avoid converting numeric values to shorter forms (e.g., 0.5 to .5)
+          removeXMLNS: false, // avoid removing the xmlns attribute (important for SVGs to be valid)
+          removeDesc: false, // avoid removing the desc attribute/element
+        },
+      },
+    },
+    // disable plugins that cause issues with the SVGs
+    {
+        name: 'cleanupListOfValues',
+        active: false
+    }
+  ],
+};
